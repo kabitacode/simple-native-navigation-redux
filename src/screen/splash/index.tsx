e4e03a2bfc1931745236956f4222a93e24cd747b
Muhamad Zulfiqor <siapaa249@gmail.com>
2023-12-05 09:20:24 +0700

    ✨ add camera

--- src/screen/splash/index.tsx
+++ src/screen/splash/index.tsx
@@ -1,6 +1,6 @@
 import { NativeBaseProvider, Spinner, useToast } from "native-base"
 import React, { useEffect } from "react"
-import { SafeAreaView, View } from 'react-native'
+import { SafeAreaView, StyleSheet, View } from 'react-native'
 import { Navigation } from "react-native-navigation";
 import { useSelector } from "react-redux"
 import { nativeViewGestureHandlerProps } from "react-native-gesture-handler/lib/typescript/handlers/NativeViewGestureHandler";
@@ -10,34 +10,48 @@ import { reduxProvider } from "../../config/store";
 import Home from "../home";
 import { startApp } from '../../authFlow';
 import { startApp as noAuth } from '../../noAuth';
+import { useCameraPermission, useCameraDevice, Camera, useCodeScanner } from "react-native-vision-camera";
 
 function Splash() {
-    const { data, loading, authenticated } = useSelector(s => s.auth);
-    const toast = useToast();
+    const { hasPermission, requestPermission } = useCameraPermission()
+    const device = useCameraDevice('back')
 
     useEffect(() => {
-        if (data?.message == 'Invalid credentials') {
-            toast.show({
-                description: "Invalid credentials!"
-            })
+      requestPermission()
+    
+      return () => {
+        
+      }
+    }, [])
+    
+    const codeScanner = useCodeScanner({
+        codeTypes: ['qr', 'ean-13'],
+        onCodeScanned: (codes) => {
+            const value = codes[0]?.value
+            if (value == null) return
+            console.log(value);
         }
+      })
 
-        if (authenticated) {
-           startApp()
-        } else {
-           noAuth()
-        }
-        return () => {}
-    }, [data, authenticated])
-
-
-    return (
-        <NativeBaseProvider>
-            <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#ccc999' }}>
-                <Spinner color="emerald.500" size={'lg'} />
-            </SafeAreaView>
-        </NativeBaseProvider>
-    )
+    if (device == null && hasPermission == false) {
+        return (
+            <NativeBaseProvider>
+                <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#ccc999' }}>
+                    <Spinner color="emerald.500" size={'lg'} />
+                </SafeAreaView>
+            </NativeBaseProvider>
+        )
+    } else {
+        return (
+            <Camera
+                style={StyleSheet.absoluteFill}
+                device={device}
+                isActive={true}
+                codeScanner={codeScanner}
+            />
+        )
+    }
+  
 }
 
 export default Splash
